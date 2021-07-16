4e34127c87ddbd8d1509355c66bb6efe89d8edee
Jack Durham <jackwdurham@yahoo.co.uk>
2021-07-16 17:45:02 -0500

    added info for auth0_id in dynamo and sample curl

--- readme.md
+++ readme.md
@@ -9,4 +9,4 @@
 # Usage
 
 sample command to add a user to the `users` dynamoDB:
-`curl -X PUT -H "Content-Type: application/json" -d '{"email":"jack.durham.dev@gmail.com","name":"Jack Durham","phoneNumber": "2146822126"}' https://mnf2qsqt3m.execute-api.us-east-2.amazonaws.com/default/user`
+`curl -X PUT -H "Content-Type: application/json" -d '{"email":"jack.durham.dev@gmail.com","name":"Jack Durham","phoneNumber": "2146822126","auth0_id":"cunk"}' https://mnf2qsqt3m.execute-api.us-east-2.amazonaws.com/default/user`
--- userAdd.py
+++ userAdd.py
@@ -5,9 +5,10 @@ def lambda_handler(event, context):
     name = str(event['body'].get('name'))
     email = str(event['body'].get('email'))
     phoneNumber = str(event['body'].get('phoneNumber'))
-    newUser = "name: " + name + "\temail: " + email + "\tphone number: " + phoneNumber
+    auth0_id = str(event['body'].get('auth0_id'))
+    newUser = "name: " + name + "\temail: " + email + "\tphone number: " + phoneNumber + "\t auth0_id: " + auth0_id
     print("New user info recievied")
     print(newUser)
     client = boto3.resource('dynamodb')
     table = client.Table("users")
-    table.put_item(Item={'email':email,'name':name,'phoneNumber':phoneNumber})
+    table.put_item(Item={'email':email,'name':name,'phoneNumber':phoneNumber,'auth0_id':auth0_id})
